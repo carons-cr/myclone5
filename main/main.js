ab4b01a93d1422802474f777687348d94f2693d9
carons-cr <2738794789@qq.com>
2017-06-13 11:04:53 +0800

    求数组最小值平均值等

--- main/main.js
+++ main/main.js
@@ -1,18 +1,43 @@
-module.exports = function main() {
+﻿module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
-};
+    	    console.log('o) 最小值 = '+sequence.minimum()
+    	    +'o) 最大值 = '+sequence.maxmum()
+    	    +'o) 元素数量 = '+sequence.count()
+    	    +'o) 平均值 = '+sequence.avg());  	   
+    };
 
-class Sequence {
-  constructor(input) {
-    // Write your code here
-  }
-
-  minimum() {
-    // Write your code here
-  }
-
-  // Write your code here
-}
+    function Sequence(input){
+	    	  var array=new Array();
+	    	  array=input;
+	    	  Sequence.prototype.minimum=function() {
+		    	       var minimum=array[0];
+		    	       for(var i=0;i<array.length;i++){
+		    	    	    if(array[i]<minimum){
+		    	    	    	minimum=array[i];
+		    	    	    }
+		    	       }
+		    	       return minimum;
+	    	  };
+	    	  Sequence.prototype.maxmum=function(){
+		    		  var maxmum=array[0];
+		    	       for(var i=0;i<array.length;i++){
+		    	    	    if(array[i]>maxmum){
+		    	    	    	maxmum=array[i];
+		    	    	    }
+		    	       }
+		    	       return maxmum;
+	    	  };
+	    	  Sequence.prototype.count=function(){
+		    		   var count=array.length;
+		    		   return count;
+	    	  }
+	    	  Sequence.prototype.avg=function(){
+	    		      var sum=0;
+	    		      var avg=0;
+		    		  for(var i=0;i<array.length;i++){
+		    	    	     sum=sum+array[i];
+		    	       }
+		    		  avg=(sum/(array.length)).toFixed(2);
+		    		  return avg;
+	    	  };
+  	};
