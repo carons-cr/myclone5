8a7b9e765ca98c7c1596359492a2290866d2c2df
carons-cr <2738794789@qq.com>
2017-06-13 11:46:04 +0800

    直接在字符串里换行

--- main/main.js
+++ main/main.js
@@ -1,9 +1,9 @@
 ﻿module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     	    console.log('o) 最小值 = '+sequence.minimum()
-    	    +'o) 最大值 = '+sequence.maxmum()
-    	    +'o) 元素数量 = '+sequence.count()
-    	    +'o) 平均值 = '+sequence.avg());  	   
+    	    +'\no) 最大值 = '+sequence.maxmum()
+    	    +'\no) 元素数量 = '+sequence.count()
+    	    +'\no) 平均值 = '+sequence.avg());  	   
     };
 
     function Sequence(input){
@@ -16,7 +16,6 @@
 		    	    	    	minimum=array[i];
 		    	    	    }
 		    	       }
-                               print("\n");
 		    	       return minimum;
 	    	  };
 	    	  Sequence.prototype.maxmum=function(){
@@ -26,12 +25,10 @@
 		    	    	    	maxmum=array[i];
 		    	    	    }
 		    	       }
-                               print("\n");
 		    	       return maxmum;
 	    	  };
 	    	  Sequence.prototype.count=function(){
 		    		   var count=array.length;
-                                   print("\n");
 		    		   return count;
 	    	  }
 	    	  Sequence.prototype.avg=function(){
@@ -41,7 +38,6 @@
 		    	    	     sum=sum+array[i];
 		    	       }
 		    		  avg=(sum/(array.length)).toFixed(2);
-                                  print("\n");
 		    		  return avg;
 	    	  };
   	};
